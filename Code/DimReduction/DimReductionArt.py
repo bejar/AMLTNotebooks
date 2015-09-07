5af53cd30f69ebde5eb6a8fa3660c7dc9a408b48
bejar <bejar@lsi.upc.edu>
2015-09-07 15:55:22 +0200

    Adding dimensionality reduction code and other fixes

---
+++ Code/DimReduction/DimReductionArt.py
@@ -0,0 +1,95 @@
+"""
+.. module:: DimReductionArt
+
+DimReductionArt
+*************
+
+:Description: DimReductionArt
+
+    
+
+:Authors: bejar
+    
+
+:Version: 
+
+:Created on: 07/09/2015 15:38 
+
+"""
+
+__author__ = 'bejar'
+
+from sklearn.datasets import load_digits
+from pylab import *
+#import seaborn as sns
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
+from amltlearn.datasets import make_blobs
+
+
+blobs, labels = make_blobs(n_samples=200, n_features=3, centers=[[1,1,1],[0,0,0],[-1,-1,-1]], cluster_std=[0.2,0.1,0.3])
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+plt.scatter(blobs[:, 0], blobs[:, 1], zs=blobs[:, 2], c=labels, s=100)
+
+
+plt.show()
+
+
+pca = PCA()
+fdata = pca.fit_transform(blobs)
+
+print pca.explained_variance_ratio_
+
+fig = plt.figure()
+
+plt.plot(range(len(pca.explained_variance_ratio_)), pca.explained_variance_ratio_)
+plt.show()
+
+#
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+plt.scatter(fdata[:, 0], fdata[:, 1], zs=fdata[:, 2], c=labels,s=100)
+
+plt.show()
+
+
+# ISOMAP
+
+from sklearn.manifold import Isomap
+iso = Isomap(n_components=3, n_neighbors=30)
+fdata = iso.fit_transform(blobs)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+plt.scatter(fdata[:, 0], fdata[:, 1], zs= fdata[:, 2], c=labels, s=100)
+
+plt.show()
+
+# LLE
+
+from sklearn.manifold import LocallyLinearEmbedding
+lle = LocallyLinearEmbedding(n_neighbors=15, n_components=3, method='modified')
+fig = plt.figure()
+fdata = lle.fit_transform(blobs)
+ax = fig.add_subplot(111, projection='3d')
+
+plt.scatter(fdata[:, 0], fdata[:, 1], zs= fdata[:, 2], c=labels, s=100)
+
+plt.show()
+
+# MDS
+
+from sklearn.manifold import MDS
+
+mds = MDS(n_components=3)
+fig = plt.figure()
+fdata = mds.fit_transform(blobs)
+ax = fig.add_subplot(111, projection='3d')
+
+plt.scatter(fdata[:, 0], fdata[:, 1], zs= fdata[:, 2], c=labels, s=100)
+
+plt.show()
--- Code/Graphs/DenseSubgraphs.py
+++ Code/Graphs/DenseSubgraphs.py
@@ -27,7 +27,7 @@ from geojson import LineString, FeatureCollection, Feature
 import geojson
 
 
-fname = './Data/bcntwitter2.txt'
+fname = './Data/paristwitter.txt'
 
 
 rfile = open(fname, 'r')
@@ -42,7 +42,7 @@ for lines in rfile:
             if v1 != v2:
                 gr.add_edge(v1,v2)
 
-coord = (41.20, 41.65, 1.90, 2.40) #(51.23, 51.8, -0.5, 0.37)
+coord = (48.52, 49.05, 1.97, 2.68) #(51.23, 51.8, -0.5, 0.37) #(41.20, 41.65, 1.90, 2.40)
 
 
 # nx.draw_spring(gr)
@@ -54,7 +54,7 @@ a = nx.find_cliques(gr)
 
 m=0
 for c in a:
-    if len(c)>20:
+     if len(c)>10:
         print len(c)
         m += 1
         seq = []
@@ -74,14 +74,14 @@ for c in a:
                     lgeo.append(Feature(geometry=LineString([(y1, x1), (y2, x2)])))
         geoc = FeatureCollection(lgeo)
         dump = geojson.dumps(geoc)
-        jsfile = open('/home/bejar/tmp/Map' + str(m) + '.json', 'w')
+        jsfile = open('/home/bejar/tmp/Map' + str(m) + '-' + str(len(c)) + '.json', 'w')
         jsfile.write(dump)
         jsfile.close()
         mymap = folium.Map(location=[(coord[0] + coord[1]) / 2.0, (coord[2] + coord[3]) / 2.0], zoom_start=12, width=1200,
                            height=1000)
-        mymap.geo_json(geo_path='/home/bejar/tmp/Map' + str(m) + '.json', fill_color='Black', line_color='Black',
+        mymap.geo_json(geo_path='/home/bejar/tmp/Map' + str(m) + '-' + str(len(c)) + '.json', fill_color='Black', line_color='Black',
                        line_weight=2)
-        mymap.create_map(path='/home/bejar/tmp/Map' + str(m) + '.html')
+        mymap.create_map(path='/home/bejar/tmp/Map' + str(m) + '-' + str(len(c)) + '.html')
 
 # nx.draw_circular(a)
 # plt.show()
