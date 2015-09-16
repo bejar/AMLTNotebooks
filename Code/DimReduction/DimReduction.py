7c53a50ec938b88dcd70b1acd8dbf0a54b02a750
bejar <bejar@lsi.upc.edu>
2015-09-16 09:00:17 +0200

    Improving Authors and wheel datasets examples

--- Code/DimReduction/DimReduction.py
+++ Code/DimReduction/DimReduction.py
@@ -30,22 +30,22 @@ digits = load_digits()
 
 # PCA transformation
 
-# pca = PCA()
-# fdata = pca.fit_transform(digits['data'])
-#
-# print pca.explained_variance_ratio_
-#
-# fig = plt.figure()
-#
-# plt.plot(range(len(pca.explained_variance_ratio_)), pca.explained_variance_ratio_)
-# plt.show()
-#
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# plt.scatter(fdata[:, 0], fdata[:, 1], zs= fdata[:, 2], c=digits['target'],s=100)
-#
-# plt.show()
+pca = PCA()
+fdata = pca.fit_transform(digits['data'])
+
+print pca.explained_variance_ratio_
+
+fig = plt.figure()
+
+plt.plot(range(len(pca.explained_variance_ratio_)), pca.explained_variance_ratio_)
+plt.show()
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+plt.scatter(fdata[:, 0], fdata[:, 1], zs= fdata[:, 2], c=digits['target'],s=100)
+
+plt.show()
 
 #
 # from sklearn.decomposition import KernelPCA
@@ -62,28 +62,28 @@ digits = load_digits()
 
 # ISOMAP
 
-# from sklearn.manifold import Isomap
-# iso = Isomap(n_components=3, n_neighbors=15)
-# fdata = iso.fit_transform(digits['data'])
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-#
-# plt.scatter(fdata[:, 0], fdata[:, 1], zs= fdata[:, 2], c=digits['target'], s=100)
-#
-# plt.show()
+from sklearn.manifold import Isomap
+iso = Isomap(n_components=3, n_neighbors=15)
+fdata = iso.fit_transform(digits['data'])
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+plt.scatter(fdata[:, 0], fdata[:, 1], zs= fdata[:, 2], c=digits['target'], s=100)
+
+plt.show()
 
 
 # LLE
 
-# from sklearn.manifold import LocallyLinearEmbedding
-# lle = LocallyLinearEmbedding(n_neighbors=15, n_components=3, method='modified')
-# fig = plt.figure()
-# fdata = lle.fit_transform(digits['data'])
-# ax = fig.add_subplot(111, projection='3d')
-#
-# plt.scatter(fdata[:, 0], fdata[:, 1], zs= fdata[:, 2], c=digits['target'], s=100)
-#
-# plt.show()
+from sklearn.manifold import LocallyLinearEmbedding
+lle = LocallyLinearEmbedding(n_neighbors=15, n_components=3, method='modified')
+fig = plt.figure()
+fdata = lle.fit_transform(digits['data'])
+ax = fig.add_subplot(111, projection='3d')
+
+plt.scatter(fdata[:, 0], fdata[:, 1], zs= fdata[:, 2], c=digits['target'], s=100)
+
+plt.show()
 
 # MDS
 
