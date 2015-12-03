c8203f052ae1a37cddafde6e9afdc81dfc0cddf6
bejar <bejar@lsi.upc.edu>
2015-12-03 14:50:22 +0100

    Some Updating

---
+++ Code/Sequential/__init__.py
@@ -0,0 +1,20 @@
+"""
+.. module:: __init__.py
+
+__init__.py
+*************
+
+:Description: __init__.py
+
+    
+
+:Authors: bejar
+    
+
+:Version: 
+
+:Created on: 26/10/2015 10:41 
+
+"""
+
+__author__ = 'bejar'
