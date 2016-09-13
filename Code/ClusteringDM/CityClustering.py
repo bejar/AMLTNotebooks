bf2a1d034fef087b33cea48f3cf198112b91e58a
javier.bejar <bejar@cs.upc.edu>
2016-09-13 11:51:28 +0200

    Some Updating

--- Code/ClusteringDM/CityClustering.py
+++ Code/ClusteringDM/CityClustering.py
@@ -24,6 +24,8 @@ import numpy as np
 from numpy import loadtxt
 import time
 from sklearn.metrics import adjusted_mutual_info_score
+import warnings
+warnings.filterwarnings("ignore")
 
 citypath = '/home/bejar/Data/City/'
 
@@ -32,32 +34,32 @@ data = 'LONcrime.csv'
 
 citypos = loadtxt(citypath+data, delimiter=',')
 
-print(citypos.shape)
+print(data, citypos.shape)
 
 # KMeans
 km = KMeans(n_clusters=100, n_init=1)
 itime = time.perf_counter()
 kmlabels = km.fit_predict(citypos)
 etime = time.perf_counter()
-print (etime-itime)
+print ('K-means Time = ', etime-itime)
 
 # Minibatch Kmeans
 itime = time.perf_counter()
 mbkm = MiniBatchKMeans(n_clusters=100, batch_size=1000, n_init=1, max_iter=5000)
 mbkmlabels = mbkm.fit_predict(citypos)
 etime = time.perf_counter()
-print (etime-itime)
+print ('MB K-means Time = ', etime-itime)
 
-print(adjusted_mutual_info_score(kmlabels, mbkmlabels))
+print('Similarity Km vs MBKm', adjusted_mutual_info_score(kmlabels, mbkmlabels))
 
 # Birch
 itime = time.perf_counter()
 birch = Birch(threshold=0.02, n_clusters=100, branching_factor=100)
 birchlabels = birch.fit_predict(citypos)
 etime = time.perf_counter()
-print (etime-itime)
+print ('BIRCH Time = ',etime-itime)
 
-print(adjusted_mutual_info_score(kmlabels, birchlabels))
+print('Similarity Km vs BIRCH',adjusted_mutual_info_score(kmlabels, birchlabels))
 
 
 
